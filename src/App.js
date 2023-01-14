3e325052da3a11eb3b44d61235c1546078e5975b
Stefigo <122674068+Stefigo@users.noreply.github.com>
2023-01-14 13:38:33 +0100

    starting navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <TextLinkExample/>
       <header className="App-header">
       </header>
     </div>
+  );
+}
+
+
+function TextLinkExample() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
